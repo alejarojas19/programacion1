3319b8ca387e7b687412d92164133cf61e711776
alejarojas19 <mariarojas0119@hotmail.com>
2021-05-06 06:50:14 -0500

    graficos

---
+++ talleres/tallerGraficos/torta.py
@@ -0,0 +1,4 @@
+import matplotlib.pyplot as plt
+pieLabels = ["Medellin", "Bogota", "Armenia", "Cali"] #Etiquetas
+sizes = [30,45, 10, 15] #Tamaño de cada porcion de torta
+pieExplode = [0, 0.3, 0, 0] #que tan alejado esta del origen
